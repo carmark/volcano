a1722e96ad298306ceb7e1f22b000cea522505bb
Xiang Dai <long0dai@foxmail.com>
2020-05-26 11:33:42 +0800

    Lint: fix structcheck issue
    Signed-off-by: Xiang Dai <long0dai@foxmail.com>

--- pkg/scheduler/cache/cache.go
+++ pkg/scheduler/cache/cache.go
@@ -79,7 +79,7 @@ type SchedulerCache struct {
 
 	podInformer             infov1.PodInformer
 	nodeInformer            infov1.NodeInformer
-	nsInformer              infov1.NamespaceInformer
+	_                       infov1.NamespaceInformer
 	podGroupInformerV1beta1 vcinformerv1.PodGroupInformer
 	queueInformerV1beta1    vcinformerv1.QueueInformer
 	pvInformer              infov1.PersistentVolumeInformer
